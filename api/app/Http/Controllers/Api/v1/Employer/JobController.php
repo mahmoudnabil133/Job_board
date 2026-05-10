a7053bbe795576515d879cce2c0b9a52ad4c64cb
mahmoudnabil133 <hodanabil155@gmail.com>
2026-05-11 00:43:23 +0300

    feat: refactor job CRUD operations for employer

--- api/app/Http/Controllers/Api/v1/Employer/JobController.php
+++ api/app/Http/Controllers/Api/v1/Employer/JobController.php
@@ -7,23 +7,23 @@ use App\Http\Requests\Job\StoreJobRequest;
 use App\Http\Requests\Job\UpdateJobRequest;
 use App\Http\Resources\JobListResource;
 use App\Http\Resources\JobResource;
+use App\Services\JobService;
 use App\Models\Job;
 use App\Models\User;
-use Illuminate\Http\Request;
-use JobService;
+
 
 class JobController extends Controller
 {
-    public function __construct(private JobService $jobService){}
+    public function __construct(private  JobService $jobService){}
     
     public function index(){
-        $user = User::firstOrFail(); // replace it with auth()->user() later
+        $user = auth()->user(); // replace it with auth()->user() later
         $jobs = Job::where('employer_id', $user->id)->with(['company', 'category', 'skills'])->latest()->paginate(10);
         return JobListResource::collection($jobs);
-    }
+    }   
 
     public function store(StoreJobRequest $request){
-        $user = User::firstOrFail(); // replace it with auth()->user() later
+        $user = auth()->user(); // replace it with auth()->user() later
         $job = $this->jobService->create($request->validated(), $user);
         return new JobResource($job);
     }
@@ -34,13 +34,13 @@ class JobController extends Controller
     }
 
     public function update(UpdateJobRequest $request, Job $job){
-        $user = User::firstOrFail(); // replace it with auth()->user() later
+        $user = auth()->user(); // replace it with auth()->user() later
         $updatedJob = $this->jobService->update($job, $request->validated(), $user);
         return new JobResource($updatedJob);
     }
 
     public function destroy(Job $job){
-        $user = User::firstOrFail(); // replace it with auth()->user() later
+        $user = auth()->user(); // replace it with auth()->user() later
         $this->jobService->delete($job, $user);
         return response()->json(['message' => 'Job deleted successfully']);
     }
